7bdc46f70f9314b1b63f50d842e043e18b6448a9
github username <github email address>
2017-12-18 12:44:34 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_reader :length_one, :length_two, :length_three
+  def initialize(length_one,length_two,length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+  if (@length_one + @length_two) <= @length_three || (@length_one + @length_three) <= @length_two || (@length_two + @length_three) <= @length_one
+
+    raise TriangleError
+  elsif @length_one <= 0 || @length_two <= 0 || @length_three <=  0
+
+      raise TriangleError
+
+
+
+    elsif @length_one == @length_two && @length_two == @length_three && @length_three == @length_one
+      :equilateral
+    elsif @length_one == @length_two || @length_one == @length_three || @length_two == @length_three
+      :isosceles
+    elsif @length_one != @length_two && @length_one != @length_three && @length_two != @length_three && @length_three != @length_one
+      :scalene
+
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
