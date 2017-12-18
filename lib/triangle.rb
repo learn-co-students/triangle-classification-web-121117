ad379a65ab91cda29465d6fd939179e85faa8a15
josephdlawson21 <josephdlawson21@gmail.com>
2017-12-17 21:02:43 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    case
+    when a == 0 && b == 0 && c == 0
+      raise TriangleError
+    when a < 0 || b < 0 || c < 0
+      raise TriangleError
+    when !(a+b>c && a+c>b && b+c>a)
+      raise TriangleError
+    end
+  end
+
+  def kind
+    case
+    when @a==@b && @a==@c && @b==@c
+      return :equilateral
+    when @a==@b || @a==@c || @b==@c
+      return :isosceles
+    when @a!=@b && @a!=@c && @b!=@c
+      return :scalene
+    end
+  end
+end
+
+
+class TriangleError < StandardError
+  def message
+    "the triangle is wrong"
+  end
+
 end
